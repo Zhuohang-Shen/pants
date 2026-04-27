91fabbcc3ac59c936c475cd94398ce2ed90eefb5
Tom Dyas <tom@shoalsoftware.com>
2026-04-27 09:47:25 +0200

    remove future import and newline for block sorting

--- src/python/pants/backend/observability/opentelemetry/opentelemetry_integration_test.py
+++ src/python/pants/backend/observability/opentelemetry/opentelemetry_integration_test.py
@@ -1,8 +1,6 @@
 # Copyright 2026 Pants project contributors (see CONTRIBUTORS.md).
 # Licensed under the Apache License, Version 2.0 (see LICENSE).
 
-from __future__ import annotations
-
 import json
 import logging
 import threading
